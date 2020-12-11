890ca7aea9658f1c569e6ac8659ea3a87b283fd3
Flávio Micheletti <flavio.micheletti@midway.com.br>
2020-12-10 22:32:08 -0300

    números primos

---
+++ 04-numeros-primos/app.js
@@ -0,0 +1,13 @@
+var ehPrimo = function(num) {
+    return 0;
+}
+
+// Função que descobre se dois números possuem divisão exata.
+//
+// Retorna:
+//   0 para false
+//   1 para true
+//
+var ehDivisaoExata = function(dividendo, divisor) {
+    return 0;;
+}
